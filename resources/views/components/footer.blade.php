7f386904d42acc3e81931779b963aab6f64932cb
janee06 <s2022BockovaJana@skolabaltaci.cz>
2025-01-07 07:45:18 +0100

    icons + footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,16 @@
-<!-- resources/views/components/footer.blade.php -->
-<footer class="bg-white flex justify-center items-center ">
-  <div class="text-center">
-    <p class="m-0">&copy; 2024 My Shop. All rights reserved.</p>
-  </div>
+<footer class="footer bg-white text-center py-4">
+  <p>&copy; {{ date('Y') }} My Shop. All rights reserved.</p>
 </footer>
+<style>
+.footer {
+  position: absolute; /* Nastavíme absolutní pozicování */
+  bottom: 0; /* Umístíme na spodek stránky */
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  text-align: center;
+  padding: 16px;
+  box-sizing: border-box;
+}
+</style>
 
